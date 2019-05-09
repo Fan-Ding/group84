f1e844f163a86afe5705aa35df034e9fbd7d7307
Fan-Ding <45089502+Fan-Ding@users.noreply.github.com>
2019-05-09 20:36:27 +0800

    Add files via upload

---
+++ ManagerDockSelectGUITest.java
@@ -0,0 +1,13 @@
+package gui;
+
+import module.DockList;
+
+class ManagerDockSelectGUITest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DockList dockList = new DockList();
+		ManagerDockSelectGUI managerDockSelectGUI = new ManagerDockSelectGUI(dockList);
+	}
+
+}
